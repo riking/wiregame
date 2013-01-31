49a6af0b5f93b31170b9fbc2e86b38c322a1c017
riking <rikingcoding@gmail.com>
2013-01-30 21:33:31 -0800

    PuzzleConfig now knows which the goal object is

--- python/puzzleconfig.py
+++ python/puzzleconfig.py
@@ -1,5 +1,17 @@
+# Puzzle configuration. Declares the I/O relationship between components.
 class PuzzleConfig (dict):
+    # local vars
+    # str(id) goal - the ident of the goal component
+    
+    # Constructor
+    # first arg should be the goal's ident
+    # all other args should be named with the idents
+    # other args must be either None or a 2-tuple of idents also present in the list
     def __init__(self, *args, **kwargs):
+        # Setup
+        assert(len(args) == 1)
+        self.goalId = args[0]
+        
         # Format Verification
         names = list()
         for k in kwargs:
@@ -7,7 +19,7 @@ class PuzzleConfig (dict):
         for k in kwargs:
             v = kwargs[k]
             if v == None: continue
-            if len(v) != 2: raise ValueError("Attempt to use more than 2 inputs in puzzle. More than 2 inputs is not supported.")
+            assert len(v) == 2 #only two inputs
             if v[0] not in names: raise ValueError("The identifier of "+ v[0] + " used in " + k + " was not defined.")
         # Constructor
         super(PuzzleConfig, self).__init__(kwargs)
@@ -26,9 +38,12 @@ class PuzzleConfig (dict):
             if ident in self[c]:
                 ret.add(c)
         return ret
+    
+    def getGoal(self):
+        return self.goalId
 
 # current default
-puzzle = PuzzleConfig(
+puzzle = PuzzleConfig('G',
     A=None,
     B=None,
     C=('A','A'),
@@ -42,3 +57,4 @@ if __name__ == "__main__":
     print puzzle
     print puzzle.getUsage('C')
     print puzzle.getUsage('A')
+    print repr(puzzle.goalId)
