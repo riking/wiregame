045b715edc5e7c45c563274e33ca3b8402dbcf3a
riking <rikingcoding@gmail.com>
2013-01-30 22:59:31 -0800

    Write board initialization

--- python/board.py
+++ python/board.py
@@ -1,4 +1,7 @@
-import puzzle
+import component, puzzle, wires
+
+# TODO move this somewhere, it's a constant
+SIDEBAR_IDENT_PREFIX = "SIDE_"
 
 # The grand container for the state of the game.
 # Holds the Puzzle object and a dictionary of all Components by ident.
@@ -8,8 +11,52 @@ class Board (object):
     # Puzzle puzzle - the puzzle object
     # dict<str(id), Component> components - dictionary of components by ident
     # list<Component> sidebar - components not actually in circuit. hold unused truthtables
+    # boolean lock - whether to use the extra & locked components
+    
+    # static vars
+    sidebar_suffix = 0
     
     # Constructor
-    def __init__(self, puz):
+    # @arg Puzzle puz - Puzzle object
+    # @arg boolean lock - whether the extra & locked components are to be used
+    def __init__(self, puz, lock):
         self.puzzle = puz
+        self.lock = lock
+        self._init_sidebar()
+        self._init_components()
+
+            
+    def _init_sidebar(self):
+        self.sidebar = list()
+        sidebar_tts = self.puzzle.getMovableTruthTablesShuffled(lock)
+        for tt in sidebar_tts:
+            newid = SIDEBAR_IDENT_PREFIX + str(sidebar_suffix)
+            sidebar_suffix++
+            comp = component.Component(newid, tt, None) # TODO create render info
+            sidebar.append(comp)
+    
+    def _init_components(self):
+        # Create components
+        self.components = dict.fromkeys(self.puzzle.getAllIdents())
+        for ident in self.components:
+            self.components[ident] = component.Component(
+              ident, wires.TTplaceholder(), None) # TODO create render info
         
+        # Define input relations
+        config = self.puzzle.getPuzzleConfig()
+        for ident in self.components:
+            inputs = config.getInputs(ident)
+            self.components[ident].defineInputs(inputs[0], inputs[1])
+        
+        # Fill input devices
+        inputs = config.getAllInputDevices()
+        for ident in inputs:
+            self.components[ident].setPermanentInput(self.puzzle.getDeviceInput(ident))
+        
+        # Fill locked component
+        if self.lock:
+            ident = self.puzzle.getLockedIdent()
+            self.components[ident].setTruthTable(self.puzzle.getLockedTT())
+    
+    def getComponent(self, ident):
+        return components[ident]
